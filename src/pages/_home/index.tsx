56eb463449a1f5793b805f38c11fd3711d991a3c
William Tonglet <william.tonglet@wipro.com>
2021-05-17 16:59:21 -0300

    adjusts topics GET query

--- src/pages/_home/index.tsx
+++ src/pages/_home/index.tsx
@@ -34,7 +34,7 @@ const HomePage = ({ topics, cover }: PageProps) => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data: topicsData } = await api.get('/topics?order_by=featured');
+  const { data: topicsData } = await api.get('/topics');
   const { data: coverData } = await api.get(
     '/photos/random?orientation=landscape',
   );
