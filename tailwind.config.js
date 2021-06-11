55777cba7ddc87b0a165b2b9ef9c4283ed65b00c
William Tonglet <william.tonglet@wipro.com>
2021-06-11 01:20:10 -0300

    insert lib to purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     'postcss-import',
     'tailwindcss',
     'autoprefixer',
+    [
+      '@fullhuman/postcss-purgecss',
+      process.env.NODE_ENV === 'production'
+        ? {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+    ],
   ],
 };
