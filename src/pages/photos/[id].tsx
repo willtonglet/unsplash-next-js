5bb25eddadd02182cd640ae80d33666359717211
William Tonglet <william.tonglet@wipro.com>
2021-06-11 01:13:01 -0300

    removes topics from id photo page

--- src/pages/photos/[id].tsx
+++ src/pages/photos/[id].tsx
@@ -1,41 +1,28 @@
 import { GetServerSideProps } from 'next';
 import { unsplash } from '@core/middleware/api';
+import { useRouter } from 'next/router';
 import PhotoContent from '@templates/PhotoContent';
 import ModalPhoto from '@templates/ModalPhoto';
-import { useRouter } from 'next/router';
 import SimpleMasonry from '@templates/SimpleMasonry';
 import PageWrapper from '@templates/PageWrapper';
 
-interface PhotoPageProps extends PageProps {
-  image: ImageProps;
-}
-
-const Photos = ({
-  image,
-  photos,
-  topics,
-}: PhotoPageProps): React.ReactElement => {
+const Photos = ({ cover, photos }: PageProps): React.ReactElement => {
   const router = useRouter();
   return (
-    <PageWrapper topics={topics}>
-      <PhotoContent image={image} />
+    <PageWrapper>
+      <PhotoContent image={cover} />
       <SimpleMasonry photos={photos} />
-      <ModalPhoto isOpen={router.query.id !== image.id} />
+      <ModalPhoto isOpen={router.query.id !== cover.id} />
     </PageWrapper>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const { data: image } = await unsplash.get(`/napi/photos/${query.id}`);
+  const { data: cover } = await unsplash.get(`/napi/photos/${query.id}`);
   const { data: photos } = await unsplash.get(
     `/napi/photos/${query.id}/related`,
   );
-  const { data: topics } = await unsplash.get('/napi/topics', {
-    params: {
-      per_page: 25,
-    },
-  });
-  return { props: { image, photos: photos.results, topics } };
+  return { props: { cover, photos: photos.results } };
 };
 
 export default Photos;
