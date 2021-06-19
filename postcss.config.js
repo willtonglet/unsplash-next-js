08be94e9db30c21749d93e6e2ac9cb658a25d4b9
William Tonglet <william.tonglet@wipro.com>
2021-06-19 11:07:39 -0300

    refactor main cover to keep size on load

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,3 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    [
-      '@fullhuman/postcss-purgecss',
-      process.env.NODE_ENV === 'production'
-        ? {
-            content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}',
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          }
-        : false,
-    ],
-  ],
+  plugins: ['postcss-import', 'tailwindcss', 'autoprefixer'],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  purge: {
-    content: [
-      './components/**/*.{js,ts,jsx,tsx}',
-      './pages/**/*.{js,ts,jsx,tsx}',
-    ],
-    options: {
-      safelist: {
-        standard: ['outline-none'],
-      },
-    },
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
   },
-  theme: {},
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 };
