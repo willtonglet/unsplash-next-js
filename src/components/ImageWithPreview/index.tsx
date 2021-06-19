d4c99a8e63f139674ce95f4ba6f2f8e59f7339da
William Tonglet <william.tonglet@wipro.com>
2021-06-19 15:15:47 -0300

    refactor main cover to keep size on load

--- src/components/ImageWithPreview/index.tsx
+++ src/components/ImageWithPreview/index.tsx
@@ -1,5 +1,5 @@
 import Image, { ImageProps } from 'next/image';
-import Blurhash from '../BlurHash';
+import BlurHash from '../BlurHash';
 
 type ImageWithPreviewProps = ImageProps & {
   hash: string;
@@ -16,7 +16,7 @@ const ImageWithPreview = (props: ImageWithPreviewProps): React.ReactElement => {
           className="w-full h-full absolute top-0 left-0"
           style={{ backgroundColor: color }}
         >
-          {hash && <Blurhash hash={hash} width="100%" height="100%" />}
+          {hash && <BlurHash hash={hash} width="100%" height="100%" />}
         </div>
       </div>
     </div>
