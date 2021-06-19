3c70ffd64d4f95947fff6f97b36f81cb96411627
William Tonglet <william.tonglet@wipro.com>
2021-06-19 11:32:35 -0300

    refactor main cover to keep size on load

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  plugins: ['postcss-import', 'tailwindcss', 'autoprefixer'],
-};
+import purgecss from '@fullhuman/postcss-purgecss';
+
+export const plugins = [
+  'postcss-import',
+  'tailwindcss',
+  'autoprefixer',
+  purgecss({
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
+    ],
+  }),
+];
