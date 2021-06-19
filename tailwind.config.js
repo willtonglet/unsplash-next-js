2e316af256307c0656b8aef71df0916d7ea6eb61
William Tonglet <william.tonglet@wipro.com>
2021-06-19 11:51:08 -0300

    refactor main cover to keep size on load

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  purge: false,
-  darkMode: false,
+  purge: {
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
+    ],
+
+    options: {
+      safelist: ['html', 'body'],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
