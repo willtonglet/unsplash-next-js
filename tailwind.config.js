d7c0a6ed7d69b59dbfc2bcd2d70bb7402b9357a6
William Tonglet <william.tonglet@wipro.com>
2021-06-19 10:20:39 -0300

    refactor main cover to keep size on load

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,23 +8,5 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    'postcss-import',
-    'tailwindcss',
-    'autoprefixer',
-    [
-      '@fullhuman/postcss-purgecss',
-      process.env.NODE_ENV === 'production'
-        ? {
-            content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}',
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          }
-        : false,
-    ],
-  ],
+  plugins: [],
 };
