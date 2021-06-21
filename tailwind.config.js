f96babdfe0fe1048d3be2d0ab624d1b3fbe6bafb
William Tonglet <william.tonglet@wipro.com>
2021-06-20 22:11:11 -0300

    refactor search masonry to work as windowing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   purge: [
-    './src/pages/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false,
+  darkMode: 'class',
   theme: {
     extend: {},
   },
