ae4bb22238bfce758e7d3156c8f9b8520a1be3ae
William Tonglet <william.tonglet@wipro.com>
2021-06-21 09:49:15 -0300

    refactor modal component to adjust image space

--- src/templates/PhotoContent/index.tsx
+++ src/templates/PhotoContent/index.tsx
@@ -5,12 +5,11 @@ import { StyledPhotoContent } from './styles';
 
 interface PhotoContentProps {
   image: ImageProps;
-  className?: string;
 }
 
 const PhotoContent = forwardRef<HTMLDivElement, PhotoContentProps>(
   (props: PhotoContentProps, ref): React.ReactElement => {
-    const { image, className } = props;
+    const { image } = props;
     const [isExpanded, setIsExpanded] = useState(false);
     const handleExpand = () => setIsExpanded(!isExpanded);
 
@@ -19,11 +18,18 @@ const PhotoContent = forwardRef<HTMLDivElement, PhotoContentProps>(
     }, [image]);
 
     return (
-      <StyledPhotoContent ref={ref} isExpanded={isExpanded}>
+      <StyledPhotoContent
+        ref={ref}
+        isExpanded={isExpanded}
+        className={
+          (isExpanded && 'w-full') ||
+          (image.width > image.height ? 'w-2/3' : 'w-1/3')
+        }
+      >
         <div
           role="button"
           tabIndex={0}
-          className="wrapper relative"
+          className="wrapper relative flex-grow"
           onClick={handleExpand}
           onKeyPress={handleExpand}
         >
@@ -31,14 +37,13 @@ const PhotoContent = forwardRef<HTMLDivElement, PhotoContentProps>(
             size={24}
             className="absolute text-white top-4 right-4 z-20"
           />
-
           <ImageWithPreview
             src={isExpanded ? image.urls.full : image.urls.regular}
             hash={image.blur_hash}
             alt={image.alt_description}
             width={image.width}
             height={image.height}
-            className={className}
+            layout="responsive"
             loading="eager"
             priority
           />
--- src/templates/PhotoContent/styles.ts
+++ src/templates/PhotoContent/styles.ts
@@ -3,6 +3,5 @@ import styled from 'styled-components';
 export const StyledPhotoContent = styled.div<{ isExpanded?: boolean }>`
   .wrapper {
     cursor: ${({ isExpanded }) => (isExpanded ? 'zoom-out' : 'zoom-in')};
-    width: ${({ isExpanded }) => (isExpanded ? '100%' : '560px')};
   }
 `;
