bb56eddc6232d429706da27338c835d026fef1c7
William Tonglet <william.tonglet@wipro.com>
2021-06-21 10:35:24 -0300

    refactor modal component to adjust image space

--- src/templates/MasonryCustomSection/index.tsx
+++ src/templates/MasonryCustomSection/index.tsx
@@ -66,6 +66,7 @@ const MasonryCustomSection = (
             <RenderIfVisible key={photo.id}>
               <ImageContent
                 image={photo}
+                loading="lazy"
                 onPhotoClick={(e) => {
                   setIsModalOpen(true);
                   onPhotoClick && onPhotoClick(e);
