922fc13d5bb5241f90562eb3c8c94717981a7851
William Tonglet <william.tonglet@wipro.com>
2021-06-20 21:00:00 -0300

    refactor search masonry to work as windowing

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,31 @@
 module.exports = {
-  plugins: ['postcss-import', 'tailwindcss', 'autoprefixer'],
+  plugins: [
+    'postcss-import',
+    'postcss-flexbugs-fixes',
+    'tailwindcss',
+    'autoprefixer',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './src/pages/**/*.{js,jsx,ts,tsx}',
+          './src/components/**/*.{js,jsx,ts,tsx}',
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ['html', 'body'],
+      },
+    ],
+  ],
 };
