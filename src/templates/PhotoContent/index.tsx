daa40b9c1f18820235c6c34bf20ab145dad3c33c
William Tonglet <william.tonglet@wipro.com>
2021-06-21 11:13:18 -0300

    refactor modal component to adjust image space

--- src/templates/PhotoContent/index.tsx
+++ src/templates/PhotoContent/index.tsx
@@ -44,7 +44,6 @@ const PhotoContent = forwardRef<HTMLDivElement, PhotoContentProps>(
             width={image.width}
             height={image.height}
             layout="responsive"
-            loading="lazy"
             priority
           />
         </div>
