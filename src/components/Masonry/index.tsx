6837ca5efb1fbecc860d1a809d53aa03f6c695b8
William Tonglet <william.tonglet@wipro.com>
2021-06-22 15:27:57 -0300

    implements spinner to dispatch infinite scroll effect

--- src/components/Masonry/index.tsx
+++ src/components/Masonry/index.tsx
@@ -102,30 +102,32 @@ const Masonry = (props: MasonryProps): React.ReactElement => {
     <div className="relative">
       <StyledMasonry ref={mainRef}>{renderColumns}</StyledMasonry>
       <div
-        className="w-full min-h-screen flex justify-center items-start pt-96 absolute bottom-0 left-0"
+        className="w-full min-h-screen flex justify-center items-start items-start pt-96 absolute bottom-0 left-0"
         style={{ height: differenceBetweenCols }}
       >
-        <svg
-          className="animate-spin -ml-1 mr-3 h-6 w-6 text-gray-400"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          ref={intersectionRef}
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="3"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
+        <div className="flex justify-center items-center h-screen w-full">
+          <svg
+            className="animate-spin -ml-1 mr-3 h-6 w-6 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            ref={intersectionRef}
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="3"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
       </div>
     </div>
   );
