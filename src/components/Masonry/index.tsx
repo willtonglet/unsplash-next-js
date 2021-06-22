bd42e3bb9e54e7d14656aa84ca539f44bc702644
William Tonglet <william.tonglet@wipro.com>
2021-06-22 15:43:41 -0300

    implements spinner to dispatch infinite scroll effect

--- src/components/Masonry/index.tsx
+++ src/components/Masonry/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import useMediaQuery from '@hooks/useMediaQuery';
 
 import { StyledMasonry } from './styles';
@@ -11,7 +11,6 @@ interface MasonryProps {
 
 const Masonry = (props: MasonryProps): React.ReactElement => {
   const { children, onScrollIntersection, visibleOffset = 0 } = props;
-  const [differenceBetweenCols, setDifferenceBetweenCols] = useState(0);
   const mainRef = useRef<HTMLDivElement>(null);
   const intersectionRef = useRef<SVGSVGElement>(null);
   const isXs = useMediaQuery('xs');
@@ -85,49 +84,31 @@ const Masonry = (props: MasonryProps): React.ReactElement => {
     };
   }, [intersectionRef]);
 
-  useEffect(() => {
-    const columnsHeightArr = Array.from({ length: getColumnsNumber }).map(
-      (_, index) =>
-        mainRef.current &&
-        mainRef.current.children[index].getBoundingClientRect().height,
-    );
-
-    setDifferenceBetweenCols(
-      Math.max(...(columnsHeightArr as number[])) -
-        Math.min(...(columnsHeightArr as number[])),
-    );
-  }, [children]);
-
   return (
     <div className="relative">
       <StyledMasonry ref={mainRef}>{renderColumns}</StyledMasonry>
-      <div
-        className="w-full min-h-screen flex justify-center items-start items-start pt-96 absolute bottom-0 left-0"
-        style={{ height: differenceBetweenCols }}
-      >
-        <div className="flex justify-center items-center h-screen w-full">
-          <svg
-            className="animate-spin -ml-1 mr-3 h-6 w-6 text-gray-400"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            ref={intersectionRef}
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="3"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        </div>
+      <div className="flex justify-center items-center h-screen w-full absolute left-0 bottom-0">
+        <svg
+          className="animate-spin -ml-1 mr-3 h-6 w-6 text-gray-400"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          ref={intersectionRef}
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="3"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
       </div>
     </div>
   );
