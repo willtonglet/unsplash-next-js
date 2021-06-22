be03881039311d963ed34cd38b9a2cf5ec4d2b6b
William Tonglet <william.tonglet@wipro.com>
2021-06-22 15:49:48 -0300

    implements spinner to dispatch infinite scroll effect

--- src/components/Masonry/index.tsx
+++ src/components/Masonry/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useMediaQuery from '@hooks/useMediaQuery';
 
 import { StyledMasonry } from './styles';
+import { isServer } from '@core/utils/isServer';
 
 interface MasonryProps {
   children: React.ReactNode;
@@ -10,9 +11,10 @@ interface MasonryProps {
 }
 
 const Masonry = (props: MasonryProps): React.ReactElement => {
-  const { children, onScrollIntersection, visibleOffset = 0 } = props;
+  const { children, onScrollIntersection, visibleOffset = 1000 } = props;
+  const [isLoading, setIsLoading] = useState(false);
   const mainRef = useRef<HTMLDivElement>(null);
-  const intersectionRef = useRef<SVGSVGElement>(null);
+  const intersectionRef = useRef<HTMLDivElement>(null);
   const isXs = useMediaQuery('xs');
   const isMd = useMediaQuery('md');
   const isLg = useMediaQuery('lg');
@@ -84,31 +86,44 @@ const Masonry = (props: MasonryProps): React.ReactElement => {
     };
   }, [intersectionRef]);
 
+  useEffect(() => {
+    if (isServer) window.addEventListener('scroll', () => setIsLoading(true));
+
+    return () => {
+      if (isServer)
+        window.removeEventListener('scroll', () => setIsLoading(false));
+    };
+  }, []);
+
   return (
     <div className="relative">
       <StyledMasonry ref={mainRef}>{renderColumns}</StyledMasonry>
-      <div className="flex justify-center items-center h-screen w-full absolute left-0 bottom-0">
-        <svg
-          className="animate-spin -ml-1 mr-3 h-6 w-6 text-gray-400"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          ref={intersectionRef}
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="3"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
+      <div
+        ref={intersectionRef}
+        className="flex justify-center items-center h-screen w-full absolute left-0 bottom-0"
+      >
+        {isLoading && (
+          <svg
+            className="animate-spin -ml-1 mr-3 h-6 w-6 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="3"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        )}
       </div>
     </div>
   );
