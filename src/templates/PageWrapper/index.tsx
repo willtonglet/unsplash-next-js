58ef2777b04f746bdf48f34248fe29654a743878
William Tonglet <william.tonglet@wipro.com>
2021-06-24 17:14:24 -0300

    insert favicon

--- src/templates/PageWrapper/index.tsx
+++ src/templates/PageWrapper/index.tsx
@@ -17,6 +17,20 @@ const PageWrapper = ({
         <title>Beautiful Free Images &amp; Pictures | Unsplash</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          data-react-helmet="true"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://unsplash.com/favicon-32x32.png"
+        />
+        <link
+          data-react-helmet="true"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="https://unsplash.com/favicon-16x16.png"
+        />
       </Head>
       <Header topics={topics} />
       <main className="min-h-screen bg-gray-50">{children}</main>
