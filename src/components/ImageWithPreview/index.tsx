061628f8d12ff499f634ec81c7884bb1519858e5
William Tonglet <william.tonglet@wipro.com>
2021-06-25 15:14:26 -0300

    refactor useInfoPopover to request data only when is visible on screen

--- src/components/ImageWithPreview/index.tsx
+++ src/components/ImageWithPreview/index.tsx
@@ -9,17 +9,11 @@ const ImageWithPreview = (props: ImageWithPreviewProps): React.ReactElement => {
   const { hash, color, className, ...rest } = props;
 
   return (
-    <div
-      className={`${props.layout === 'fill' ? 'h-full' : 'h-max'} ${
-        className ? ` ${className}` : ''
-      }`}
-    >
-      <div
-        className={`relative${props.layout === 'fill' ? ' h-full' : ' h-max'}`}
-      >
+    <div className={className}>
+      <div className="relative">
         <Image color={color} className="z-10" {...rest} />
         <div
-          className="w-full absolute top-0 left-0"
+          className="w-full h-full absolute top-0 left-0"
           style={{ backgroundColor: color }}
         >
           {hash && <BlurHash hash={hash} width="100%" height="100%" />}
